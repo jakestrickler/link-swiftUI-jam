1ef0cd06502f40c466f6b3223376dcbd1c69fa24
strickj5 <strickj5@miamioh.edu>
2021-02-19 10:03:34 -0500

    Add Xcode Project

---
+++ Link/Shared/LinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkApp.swift
+//  Shared
+//
+//  Created by Jacob Strickler on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct LinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
